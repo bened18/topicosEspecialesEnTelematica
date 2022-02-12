cd59ccc6644b77cf9f87faff0f71b18404055a28
bened18 <belt@eafit.edu.co>
2022-02-12 08:30:37 -0500

    no changes

--- reto_1/commands/yacurl.py
+++ reto_1/commands/yacurl.py
@@ -31,7 +31,7 @@ def cli(host, port):
     response_decode = response.decode('latin-1')
     print(response_decode)
     print("---------------------------End of Connection----------------------------------------")
-    with open("parsing.html", "w") as file:
-        file.write(response_decode)
+    # with open("parsing.html", "w") as file:
+    #     file.write(response_decode)
 
             
