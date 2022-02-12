8a2c84d38fe3a9c8f0c008daea918fac589fe6ff
bened18 <belt@eafit.edu.co>
2022-02-12 08:24:37 -0500

    implementing traffic capture in port 80

--- reto_1/commands/capture.py
+++ reto_1/commands/capture.py
@@ -1,14 +1,15 @@
+from socket import timeout
 import pyshark
 import click
 
 @click.command()
 def cli():
-    capture = pyshark.LiveCapture(interface='eth0', bpf_filter='tcp port 80')
-    # capture.sniff(timeout=10)
-    # print(capture)
-    # print('packet 3 captured', capture[3])
+    host = 'www.eltiempo.com'
+    capture = pyshark.LiveCapture(interface='eth0', bpf_filter='tcp port 80 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)')
+    print(f'host {host}')
 
-    for packet in capture.sniff_continuously(packet_count=2):
+    capture.sniff(timeout=1)
+    for packet in capture.sniff_continuously(packet_count=20):
         print('just arrived: ---------- ------------------------------------')
         print(packet)
         print('end of packet content: ---------- ---------------------------')
--- reto_1/commands/yacurl.py
+++ reto_1/commands/yacurl.py
@@ -2,16 +2,14 @@ from email.policy import default
 import socket
 import pyshark
 import click
-
 @click.command()
 @click.option("-h", "--host", prompt=True)
 @click.option("-p", "--port", type=int, prompt=True, default=80)
 def cli(host, port):
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(10)  # In seconds. Choose a value that makes sense to you
     sock.connect((host, port))
-    capture = pyshark.LiveCapture(interface='eth0')
+    capture = pyshark.LiveCapture(interface='eth0', bpf_filter='tcp port 80')
     
     message = f"GET / HTTP/1.1\r\nHost:{host}\r\nConnection: close\r\n\r\n"
     request = message.encode('utf-8')
@@ -20,29 +18,22 @@ def cli(host, port):
     while sent < len(request):
         sent = sent + sock.send(request[sent:])   # Send a portion of 'request', starting from 'sent' byte      
     response = b""
-    print("toy aqui")
-    print(capture)
-    # for packet in capture.sniff_continuously(packet_count=1):
-    #     print('---------------------------------new packet just sent--------------------------------------')
-    #     print(packet)
-    #     print('---------------------------------end of sent packet content--------------------------------')
-
+    print("-----------------------------------------------------------------------------packet sent---------------------------------------------------------------------------")
+    
+   
     while True:
         chunk = sock.recv(4096)
         if len(chunk) == 0:     # No more data received, quitting
             break
         response = response + chunk
+    print("-----------------------------------------------------------------------------packet received--------------------------------------------------------------------------")
+    capture.sniff(timeout=1)
+    for packet in capture.sniff_continuously(packet_count=1):
+        print(f"    {packet}")
     sock.close()
-    print(capture)
-
-    # for packet in capture.sniff_continuously(packet_count=1):
-    #     print('---------------------------------new packet just received-----------------------------------')
-    #     print(packet)
-    #     print('---------------------------------end of received packet content-----------------------------')
     
     response_decode = response.decode('latin-1')
 
     with open("parsing.html", "w") as file:
         file.write(response_decode)
-
     
